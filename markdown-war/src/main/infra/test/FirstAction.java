b0f3f6251b6867f04d773ac76f1dca32fa82a53c
Fred <lishan.yu@oracle.com>
2013-09-07 23:32:43 +0800

    project init

--- markdown-war/src/main/infra/test/FirstAction.java
+++ markdown-war/src/main/infra/test/FirstAction.java
@@ -1,13 +1,36 @@
 package infra.test;
 
+import com.opensymphony.xwork2.ActionSupport;
 import org.apache.log4j.Logger;
+
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.String;
 
 /**
  * This is only a test action to be called by struts2
  * @author fred
  */
-public class FirstAction{
+public class FirstAction extends ActionSupport{
     private Logger log = Logger.getLogger(FirstAction.class);
+    private String name;
+
+    @Override
+    public String execute() throws Exception {
+        if(log.isInfoEnabled()){
+            log.info("execute");
+        }
+        setName("Yu Lishan");
+        return "test";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String method(){
         if(log.isInfoEnabled()){
--- markdown-war/src/main/webapp/index.jsp
+++ markdown-war/src/main/webapp/index.jsp
@@ -1,6 +1,16 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
 <html>
+<head>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.min.css"/>
+    <script type="text/javascript" src="jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+</head>
 <body>
-<h2>Hello World!</h2><br/>
-<p>This is impetus' world</p>
+<h2>Hello <s:property value="name"/>!</h2><br/>
+
+<div class="alert alert-info">
+    <p>This is impetus' world</p>
+</div>
 </body>
 </html>
