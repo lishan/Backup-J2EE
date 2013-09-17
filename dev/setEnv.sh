4e96f4c17dbddc8aaaef22ad3007431f489b213e
Fred <yulishan3@gmail.com>
2013-09-17 16:21:34 +0800

    Revert "script add param project name impetus"
    This reverts commit d660a3607f5499e61f930cf6a44baae700fef3cb.

--- dev/setEnv.sh
+++ dev/setEnv.sh
@@ -3,5 +3,4 @@ export PROJECT_HOME=/home/fred/Code/markdown
 export PLATFORM_HOME=$PROJECT_HOME/lishan-platform
 export PROJECT_WEB_HOME=$PROJECT_HOME/markdown-war
 export TOMCAT_HOME=/home/fred/Software/apache-tomcat-7.0.42/
-export PROJECT_NAME=impetus
 echo "set env complete , project home is $PROJECT_HOME"
