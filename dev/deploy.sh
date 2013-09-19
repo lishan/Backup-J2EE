566f88431e690240e1551e7abe3cee159ce9df96
Fred <lishan.yu@oracle.com>
2013-09-19 18:16:59 +0800

    deploy

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -2,10 +2,10 @@
 . `dirname $0`/setEnv.sh
 
 pushd $TOMCAT_HOME/webapps
-	rm -rf markdown/
-	rm -rf markdown.war
+	rm -rf $PROJECT_NAME/
+	rm -rf $PROJECT_NAME.war
 popd
 
 echo "clean webapps complete!"
-cp -rf $PROJECT_WEB_HOME/target/markdown.war $TOMCAT_HOME/webapps/
+cp -rf $PROJECT_WEB_HOME/target/$PROJECT_NAME.war $TOMCAT_HOME/webapps/
 echo "depoly complete!"
