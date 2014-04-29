9e20a8f260c92e836b5c5d8b4a80a275bd2bd6d9
LishanYu <yulishan3@gmail.com>
2014-04-29 18:13:56 +0800

    [LS] git and bash init :running:

--- dev/init.sh
+++ dev/init.sh
@@ -5,11 +5,9 @@ ln -s $TOMCAT_HOME/bin/startup.sh startup.sh
 ln -s $TOMCAT_HOME/bin/shutdown.sh shutdown.sh
 ln -s $TOMCAT_HOME/bin/catalina.sh catalina.sh
 
-pushd $PROJECT_HOME
-	rm -rf .gitignore
-	touch .gitignore
-	echo "./dev/startup.sh" >> .gitignore
-	echo "./dev/shutdown.sh" >> .gitignore
-	echo "./dev/catalina.sh" >> .gitignore
-	echo "./dev/init.sh" >> .gitignore
-popd
+#pushd $PROJECT_HOME
+#	echo "*startup.sh" >> .gitignore
+#	echo "*shutdown.sh" >> .gitignore
+#	echo "*catalina.sh" >> .gitignore
+#	echo "*init.sh" >> .gitignore
+#popd
--- dev/setEnv.sh
+++ dev/setEnv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export PROJECT_HOME=/home/fred/Code/markdown
+export PROJECT_HOME=/Users/lsyu/Common/markdown
 export PLATFORM_HOME=$PROJECT_HOME/lishan-platform
-export TOMCAT_HOME=/home/fred/Software/apache-tomee-webprofile-1.5.2
+export TOMCAT_HOME=/Users/lsyu/Software/apache-tomee-webprofile-1.6.0.1
 export PROJECT_NAME=impetus
 export PROJECT_WEB_HOME=$PROJECT_HOME/$PROJECT_NAME-war
 echo "set env complete , project home is $PROJECT_HOME"
